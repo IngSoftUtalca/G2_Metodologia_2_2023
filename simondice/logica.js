2cca8ab842199012ec338737866eee2a28104b92
David Ochoa <dochoa21@alumnos.utalca.cl>
2023-12-06 21:30:38 -0300

    Refactorización y mejora del juego de Simon
    - Implementación de comentarios descriptivos para cada método y función.
    - Optimización de la lógica del juego para un rendimiento más eficiente.
    - Añadida funcionalidad de mostrar la ronda actual en el tablero.
    - Ajuste de estilos visuales para mejorar la experiencia de juego.
    - Implementación de la función de juego perdido y ganado.
    - Mejora en la generación y manejo de secuencias aleatorias de botones.
    - Habilitación y deshabilitación adecuada de botones según el estado del juego."
    además se desarrollaron los métodos:
    
    iniciar(): Inicia el juego de Simon.
    iniciarJuego(): Controla el inicio del juego, deshabilita el botón de inicio, actualiza la ronda, inicializa variables y eventos, y muestra la secuencia inicial.
    actualizarRonda(valor): Actualiza la ronda actual y el contenido visual del tablero.
    crearSecuencia(): Crea una secuencia aleatoria de botones basada en el número total de rondas.
    getColorAleatorio(): Genera un número aleatorio entre 0 y 3.
    clickBoton(valor): Función llamada cuando se hace clic en un botón.
    validarColorElegido(valor): Valida si el botón seleccionado por el usuario coincide con el valor de la secuencia.
    verificarFinJuego(): Verifica si se ha completado el juego.
    mostrarSecuencia(): Muestra la secuencia de botones que el usuario debe tocar.

--- simondice/logica.js
+++ simondice/logica.js
@@ -29,45 +29,108 @@ class JuegoSimon {
         this.interfaz.botonInicio.onclick = () => this.iniciarJuego();
     }
 
+    /**
+ * Inicia el juego, deshabilita el botón de inicio,
+ * actualiza la ronda, inicializa variables y eventos,
+ * y muestra la secuencia inicial.
+ */
     iniciarJuego() {
-        // Comienza el juego
-        // ... (inicio del juego)
+        this.interfaz.botonInicio.disabled = true;
+        this.actualizarRonda(0);
+        this.posicionUsuario = 0;
+        this.secuencia = this.crearSecuencia();
+        this.botones.forEach((elemento, i) => {
+            elemento.classList.remove('ganador');
+            elemento.onclick = () => this.clickBoton(i);
+        });
+        this.mostrarSecuencia();
     }
 
+    /**
+     * Actualiza la ronda actual y el contenido visual del tablero.
+     * @param {number} valor - El valor de la nueva ronda.
+     */
     actualizarRonda(valor) {
-        // Actualiza la ronda y el tablero
-        // ... (actualización de la ronda)
+        this.ronda = valor;
+        this.interfaz.tablero.style.color = 'red';
+        this.interfaz.tablero.style.zIndex = '9999';
+        this.interfaz.tablero.textContent = `NIVEL:  ${this.ronda}`;
     }
 
+    /**
+     * Crea una secuencia aleatoria de botones basada en el número total de rondas.
+     * @returns {number[]} - Array con la secuencia aleatoria.
+     */
     crearSecuencia() {
-        // Crea una secuencia aleatoria de botones
-        // ... (creación de secuencia aleatoria)
+        return Array.from({ length: this.totalRondas }, () => this.getColorAleatorio());
     }
 
+    /**
+     * Genera un número aleatorio entre 0 y 3.
+     * @returns {number} - Número aleatorio entre 0 y 3.
+     */
     getColorAleatorio() {
-        // Genera un número aleatorio entre 0 y 3
-        // ... (obtención de color aleatorio)
+        return Math.floor(Math.random() * 4);
     }
 
+    /**
+     * Función llamada cuando se hace clic en un botón.
+     * @param {number} valor - Valor del botón seleccionado.
+     */
     clickBoton(valor) {
-        // Ejecuta una función cuando se hace click en un botón
-        // ... (manejo de click en botón)
+        !this.botonesBloqueados && this.validarColorElegido(valor);
     }
 
+    /**
+     * Valida si el botón seleccionado por el usuario coincide con el valor de la secuencia.
+     * @param {number} valor - Valor del botón seleccionado por el usuario.
+     */
     validarColorElegido(valor) {
-        // Valida si el botón que toca el usuario coincide con el valor de la secuencia
-        // ... (validación del color elegido)
+        if (this.secuencia[this.posicionUsuario] === valor) {
+            this.sonidosBotones[valor].play();
+            if (this.ronda === this.posicionUsuario) {
+                this.actualizarRonda(this.ronda + 1);
+                this.velocidad /= 1.02;
+                this.verificarFinJuego();
+            } else {
+                this.posicionUsuario++;
+            }
+        } else {
+            this.juegoPerdido();
+        }
     }
 
+    /**
+     * Verifica si se ha completado el juego.
+     */
     verificarFinJuego() {
-        // Verifica si se ha acabado el juego
-        // ... (verificación del fin del juego)
+        if (this.ronda === this.totalRondas) {
+            this.juegoGanado();
+        } else {
+            this.posicionUsuario = 0;
+            this.mostrarSecuencia();
+        }
     }
 
+    /**
+     * Muestra la secuencia de botones que el usuario debe tocar.
+     */
     mostrarSecuencia() {
-        // Muestra la secuencia de botones que el usuario debe tocar
-        // ... (muestra la secuencia)
+        this.botonesBloqueados = true;
+        let indiceSecuencia = 0;
+        let temporizador = setInterval(() => {
+            const boton = this.botones[this.secuencia[indiceSecuencia]];
+            this.sonidosBotones[this.secuencia[indiceSecuencia]].play();
+            this.cambiarEstiloBoton(boton);
+            setTimeout(() => this.cambiarEstiloBoton(boton), this.velocidad / 2);
+            indiceSecuencia++;
+            if (indiceSecuencia > this.ronda) {
+                this.botonesBloqueados = false;
+                clearInterval(temporizador);
+            }
+        }, this.velocidad);
     }
+
 
     cambiarEstiloBoton(boton) {
         // Cambia el estilo de los botones cuando se muestra la secuencia
